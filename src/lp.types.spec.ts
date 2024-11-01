34322298c530b26c27ce6c8da60268dd45311757
Wilfred Springer <wilfred@eastpole.nl>
2024-11-01 18:33:35 +0100

    Add a few more tests

--- src/lp.types.spec.ts
+++ src/lp.types.spec.ts
@@ -74,5 +74,106 @@ Binary
  IsActive`>;
       verify<Identical<Name, "ProductA" | "ProductB" | "IsActive">>;
     });
+
+    it("should support slightly larger models", () => {
+      type Name = VariableNameOf<`Maximize
+ obj: 3 x1 + 5 x2 + 2 x3 - x4 + 7 x5
+
+Subject To
+ demand_1: 2 x1 + 3 x2 + 4 x3 + x4 <= 20
+ demand_2: 5 x1 + 2 x2 + 3 x5 >= 15
+ demand_3: x2 + 4 x3 + 2 x4 + 3 x5 <= 18
+ machine_capacity: x1 + x3 + x5 <= 10
+ manpower_limit: 4 x2 + 3 x4 + 2 x5 <= 25
+ max_orders: x1 + x2 + x3 + x4 + x5 <= 30
+ min_orders: x1 + x2 >= 5
+ specific_order: x3 = 3
+
+Bounds
+ 0 <= x1 <= 5
+ 2 <= x2 <= 8
+ x3 free
+ 0 <= x4 <= 10
+ x5 >= 1
+
+General
+ x4
+ x5
+
+Binary
+ is_active_1
+ is_active_2`>;
+      verify<
+        Identical<
+          Name,
+          "x1" | "x2" | "x3" | "x4" | "x5" | "is_active_1" | "is_active_2"
+        >
+      >;
+    });
+
+    it("should support a model twice as large", () => {
+      type Name = VariableNameOf<`Maximize
+ obj: 4 x1 + 6 x2 + 3 x3 + 7 x4 + 2 x5 + 5 x6 - 3 x7 + 8 x8 + x9 + 4 x10
+
+Subject To
+ demand_1: 2 x1 + 3 x2 + 4 x3 + x4 + 2 x5 <= 35
+ demand_2: 5 x1 + 2 x2 + 3 x6 + x7 >= 20
+ demand_3: x2 + 4 x3 + 2 x4 + 3 x5 + x8 <= 30
+ machine_capacity_1: x1 + x3 + x5 + x7 <= 12
+ machine_capacity_2: x2 + x4 + x6 + x8 <= 14
+ manpower_limit_1: 4 x2 + 3 x4 + 2 x5 + 5 x9 <= 40
+ manpower_limit_2: 3 x3 + x6 + 2 x8 + 3 x10 <= 28
+ min_orders_1: x1 + x2 + x3 >= 8
+ min_orders_2: x4 + x5 + x6 >= 6
+ specific_order_1: x7 = 2
+ specific_order_2: x8 = 4
+ max_orders: x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 <= 50
+
+Bounds
+ 0 <= x1 <= 6
+ 1 <= x2 <= 10
+ 0 <= x3 <= 8
+ x4 free
+ x5 >= 2
+ 0 <= x6 <= 15
+ 3 <= x7 <= 7
+ x8 free
+ 0 <= x9 <= 12
+ 2 <= x10 <= 9
+
+General
+ x4
+ x5
+ x6
+ x7
+ x8
+ x9
+ x10
+
+Binary
+ is_active_1
+ is_active_2
+ is_active_3
+ is_active_4`>;
+      verify<
+        Identical<
+          Name,
+          | "x1"
+          | "x2"
+          | "x3"
+          | "x4"
+          | "x5"
+          | "x6"
+          | "x7"
+          | "x8"
+          | "x9"
+          | "x10"
+          | "is_active_1"
+          | "is_active_2"
+          | "is_active_3"
+          | "is_active_4"
+        >
+      >;
+    });
   });
 });
